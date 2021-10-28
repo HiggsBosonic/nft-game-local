a4ef40e57b481c1de5216103800f4fc8f1e3db0f
HiggsBosonic <natstonecipher@gmail.com>
2021-10-28 12:54:39 -0400

    select char indes.js setup

--- src/App.js
+++ src/App.js
@@ -155,28 +155,6 @@ useEffect(() => {
   }
 }, [currentAccount]);
 
-// UseEffect
-useEffect(() => {
-  const { ethereum } = window;
-
-  if (ethereum) {
-    const provider = new ethers.providers.Web3Provider(ethereum);
-    const signer = provider.getSigner();
-    const gameContract = new ethers.Contract(
-      CONTRACT_ADDRESS,
-      myEpicGame.abi,
-      signer
-    );
-
-    /*
-     * This is the big difference. Set our gameContract in state.
-     */
-    setGameContract(gameContract);
-  } else {
-    console.log('Ethereum object not found');
-  }
-}, []);
-
   return (
     <div className="App">
       <div className="container">
--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -13,6 +13,64 @@ const SelectCharacter = ({ setCharacterNFT }) => {
       <h2>Mint Your Hero. Choose wisely.</h2>
     </div>
   );
+// UseEffect
+useEffect(() => {
+  const { ethereum } = window;
+
+  if (ethereum) {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicGame.abi,
+      signer
+    );
+
+    /*
+     * This is the big difference. Set our gameContract in state.
+     */
+    setGameContract(gameContract);
+  } else {
+    console.log('Ethereum object not found');
+  }
+}, []);
+
+useEffect(() => {
+  const getCharacters = async () => {
+    try {
+      console.log('Getting contract characters to mint');
+
+      /*
+       * Call contract to get all mint-able characters
+       */
+      const charactersTxn = await gameContract.getAllDefaultCharacters();
+      console.log('charactersTxn:', charactersTxn);
+
+      /*
+       * Go through all of our characters and transform the data
+       */
+      const characters = charactersTxn.map((characterData) =>
+        transformCharacterData(characterData)
+      );
+
+      /*
+       * Set all mint-able characters in state
+       */
+      setCharacters(characters);
+    } catch (error) {
+      console.error('Something went wrong fetching characters:', error);
+    }
+  };
+
+  /*
+   * If our gameContract is ready, let's get characters!
+   */
+  if (gameContract) {
+    getCharacters();
+  }
+}, [gameContract]);
+
+
 };
 
 export default SelectCharacter;
