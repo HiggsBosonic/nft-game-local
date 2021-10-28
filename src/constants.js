cf47a5c9d35592866403f226e0efba5207bd6da2
HiggsBosonic <natstonecipher@gmail.com>
2021-10-28 12:42:55 -0400

    initial commit

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0x6e49f091c14D147C3a77913DdBe1B2E2e789Af8a';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
